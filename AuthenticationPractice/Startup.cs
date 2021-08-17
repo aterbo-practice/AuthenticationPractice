c3cd062f130189261a05a33b1708bea58b4f72b6
aterbo <andy.terbovich@gmail.com>
2021-08-17 12:41:45 -0700

    added hardcode to create admin account upon visiting super-secret page. Test authorization attributes applied to pages.

--- AuthenticationPractice/Startup.cs
+++ AuthenticationPractice/Startup.cs
@@ -35,6 +35,7 @@ namespace AuthenticationPractice
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
